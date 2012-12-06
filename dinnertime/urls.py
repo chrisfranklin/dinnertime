49838fb1f9094fe928fbaf3de4baf65b554d1ac1
Chris Franklin <chris@piemonster.me>
2012-12-06 00:21:58 +0000

    revert last commit

--- dinnertime/urls.py
+++ dinnertime/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
     # Userena
     #==========================================================
     (r'^accounts/', include('userena.urls')),
-    (r'^lazysignup/', include('lazysignup.urls')),
+    (r'^convert/', include('lazysignup.urls')),
     #==========================================================
     # End Userena
     #==========================================================
