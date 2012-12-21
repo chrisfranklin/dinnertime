a3cf65e05e0fba55138aedeed92431fb9b646c7e
Chris Franklin <chris@piemonster.me>
2012-12-21 03:43:02 +0000

    Still trying to get autocompletes to return emails, not sure if they are working, commiting so i can make 1.4 branch because im not sure im in the mood for stash

--- meals/autocomplete_light_registry.py
+++ meals/autocomplete_light_registry.py
@@ -19,7 +19,13 @@ class InviteAutocomplete(autocomplete_light.AutocompleteGenericBase):
     # Note that defining *_js_attributes in a Widget also works. Widget has
     # priority since it's the most specific.
     autocomplete_js_attributes = {
-        'placeholder': 'type a user name ...',
+        'placeholder': 'enter name or email',
     }
+
+    # def choices_for_request(self):
+    #     """ Return choices for a particular request """
+    #     print self
+    #     return super(InviteAutocomplete, self).choices_for_request(
+    #         ).exclude(extra=self.request.GET['extra'])
 
 autocomplete_light.register(Invite, InviteAutocomplete)
